c6ffa5c27f39242bb325400c5d687b559196baf4
girlwholovesbubbaleeboba <annmarienazario14@gmail.com>
2024-03-15 10:10:08 -0400

    Project

---
+++ app.py
@@ -0,0 +1,10 @@
+# python princess quiz 
+
+questions = ("","","","","","","","","","")
+
+Testoptions= ((),(),(),(),(),(),(),(),(),())
+
+Correct answers = ()
+Incorect answers = []
+Score = 0
+question_num= 0 
