ad2c4be545f7386e07aebdfd1013a8228f0e606b
girlwholovesbubbaleeboba <annmarienazario14@gmail.com>
2024-03-26 12:00:38 -0400

    almost done

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-# python princess quiz 
+questions = ("Who was the first disney princess?",
+             "What disney princess has a tiger as their companion?",
+             "Who has the biggest princess castle?",
+             "Who is the most popular disney princess?",
+             "How many sisters does ariel have?",
+             "Which princess is skilled with archery and is determined to change her fate?",
+             "Which Disney Princess is known for her long, golden hairc that has magical properties?",
+             "In Frozen, what disney princess has the ability to create ice and snow?",
+             "which princess sets sail on a daring adventure to save her island?",
+             "What is the name of the magical fairys who helps Aurora, the Sleeping Beauty, in the Disney movie?")
 
-questions = ("","","","","","","","","","")
+options= (("A. Cinderella","B. Snow White","C. Aurora","D. Belle"),
+          ("A. Mulan","B. Ariel","C. Jasmine","D. rapunzel"),
+          ("A. Cinderella","B. Belle","C. Elsa","D. snow white"),
+          ("A. Cinderella","B. belle","C. Aurora","D. Tiana"),
+          ("A. Four","B. Five","C. Six","D. Seven"),
+          ("A. Ariel","B. Snow White","C. Mulan","D. Merida"),
+          ("A. Cinderella","B. Snow White","C. Rapunzel","D. Belle"),
+          ("A. Elsa","B. anna","C. Olaf","D. Hans"),
+          ("A. Moana","B. Tiana","C. Aurora","D. Ariel"),
+          ("A. Flora","B. Fauna","C. Merryweather","D.All of the above")),
 
-Testoptions= ((),(),(),(),(),(),(),(),(),())
 
-Correct answers = ()
-Incorect answers = []
+              
+              
+Answers = ("B","C","A","A","C","D","C","A","A","D")
+Guesses = [1]
 Score = 0
 question_num= 0 
+
+for question in questions:
+    print (question)
